fb5eac6121de8c2b5bcbd5deadab933e306e07da
YLQ <2465266566@qq.com>
2018-10-16 18:13:50 +0800

    add the directory

--- src/main/java/com/how2java/App1.java
+++ src/main/java/com/how2java/App1.java
@@ -4,6 +4,6 @@ package com.how2java;
 public class App1
 {
 	public static void main(String[] args){
-		System.out.println("Hello my first java");
+		System.out.println("Hello my first java!!!");
 	}
 }
